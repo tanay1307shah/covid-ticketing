2cb8588ab6fb98d9bf1e1f6373016052abebdfcc
Binoy Subhakumar <subhakum@usc.edu>
2020-06-16 13:39:06 -0500

    store model created

--- app.py
+++ app.py
@@ -1,61 +1,65 @@
 from flask import Flask, request, Response, render_template
 import pymongo
+from flask_restplus import Api, Resource, fields
+from bson.json_util import dumps,loads
+from bson import json_util
 import json
-from bson.json_util import dumps
-app = Flask(
-    __name__, template_folder='./client')
+from models.store import storeSchema
+
+
+app = Flask( __name__, template_folder='./client')
+api = Api(app)
 table = None
 
+# storeModel = api.model('Store', {
+#     'id_store' : fields.String('ID of the store.'),
+#     'id_owner' : fields.String('ID of the store owner.'),
+#     'location' : fields.String('Address of the store.'),
+#     'name' : fields.String('Name of the store.'),
+#     'phone' : fields.String('Phone number for the store.'),
+#     'availability': fields.List(fields.DateTime('Time slots available')),
 
-@app.route("/test")
-def hello():
-    return "Hello World!"
+# })
 
-@app.route("/")
-def homePage():
-    return render_template('home.html')    
+# storeModel = api.schema.model('Store', {
+#     'properties': {
+#         'road': {
+#             'type': 'string'
+#         },
+#     },
+#     'type': 'object'
+# })
 
-#------------REST API Code----------------------
-@app.route("/products", methods=['GET', 'POST', 'DELETE'])
-def products():
-    global table
-    if table is None:
-        table = setupDB("apifortest", "products")
+address = storeSchema(api)
+languages = []
+python = {'language' : 'Python'}
+languages.append(python)
 
-    # This section checks if the incoming request is GET and 'View all' content into mlab db
-    if request.method == "GET":
+@api.route('/store')
+class Store(Resource):
+    def get(self):
+        global table
+        if table is None:
+            table = setupDB("covid-ticketing-db", "store")
         try:
             cursor = table.find()   # operation on table to get all data
-            response = list()
+            response = []
             for document in cursor: # iterate through each db result and append to a list
                 response.append(document)
             return Response('{"response":%s,"message":"Succesfully retreived all documents"}' % dumps(response), status=200, mimetype='application/json')
+
         except Exception as e:
             print("Error occured:", str(e.args))
             return Response('{"message":"Server error. Please check logs."}', status=400, mimetype='application/json')
 
-    # This section checks if the incoming request is POST and 'Add' content into mlab db
-    elif request.method == "POST":
-        try:
-            data = request.json # retrieve parameters
-            table.insert({"name": str(data["name"]), "price": str(data["price"])}) # insert into db
-            return Response('{"message":"Succesfully added."}', status=201, mimetype='application/json')
-        except Exception as e:
-            print("Error occured:", str(e.args))
-            return Response('{"message":"Server error. Please check logs."}', status=400, mimetype='application/json')
-    
-    # This section checks if the incoming request is DELETE and 'Delete all' content from mlab db
-    elif request.method == "DELETE":
-        try:
-            table.remove() # delete all entries in table
-            return Response(status=204, mimetype='application/json')
-        except Exception as e:
-            print("Error occured:", str(e.args))
-            return Response('{"message":"Server error. Please check logs."}', status=400, mimetype='application/json')
+    @api.expect(address)
+    def post(self):
+        languages.append(api.payload)
+        return {'result' : 'Language added'}, 201 
 
 
 def setupDB(db, collection):
-    uri = "mongodb://binoy123:binoy123@ds155634.mlab.com:55634/apifortest?retryWrites=false"
+    uri = "mongodb://binoy123:binoy123@ds034807.mlab.com:34807/covid-ticketing-db?retryWrites=false"
     client = pymongo.MongoClient(uri)
     db = client[db]  # identifying the db
     collection = db[collection]  # identifying the collection/table
