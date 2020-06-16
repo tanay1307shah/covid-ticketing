05d48eaa2b8d04632ca71b02fa90322345a2e9a2
Binoy Subhakumar <subhakum@usc.edu>
2020-06-16 16:02:06 -0500

    changed app.py file to main.py

---
+++ main.py
@@ -0,0 +1,45 @@
+from flask import Flask, request, Response, render_template
+from flask_restplus import Api, Resource, fields
+from bson.json_util import dumps,loads
+from models.store import storeSchema
+from helpers.setupDB import setupDB
+
+
+app = Flask( __name__, template_folder='./client')
+api = Api(app)
+
+table = setupDB("covid-ticketing-db", "store")
+# Generating schema for store
+store = storeSchema(api)
+
+@api.route('/store')
+class Store(Resource):
+    global table
+    def get(self):
+        try:
+            cursor = table.find()   # operation on table to get all data
+            response = []
+            for document in cursor: # iterate through each db result and append to a list
+                response.append(document)
+            return Response('{"response":%s,"message":"Succesfully retreived all documents"}' % dumps(response), status=200, mimetype='application/json')
+
+        except Exception as e:
+            print("Error occured:", str(e.args))
+            return Response('{"message":"Server error. Please check logs."}', status=400, mimetype='application/json')
+
+    @api.expect(store)
+    def post(self):
+        data = api.payload
+        table.insert({
+        'id_store' : data['id_store'],
+        'id_owner' : data['id_owner'],
+        'location' : data['location'],
+        'name' : data['name'],
+        'phone' : data['phone'],
+        'availability': data['availability'],
+        'reservations': data['reservations']
+            }) # insert into db
+        return Response('{"message":"Succesfully added."}', status=201, mimetype='application/json')
+
+if __name__ == "__main__":
+    app.run(debug=True)
