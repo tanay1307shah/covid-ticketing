5d8f96a58d12081a11fb71025a87fd6d286e6ee4
Binoy Subhakumar <subhakum@usc.edu>
2020-06-17 01:51:46 -0500

    changed port to 8080

--- app.py
+++ app.py
@@ -49,4 +49,4 @@ class Store(Resource):
             return Response('{"message":"Server error. Please check logs."}', status=400, mimetype='application/json')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True,host='0.0.0.0', port=8080)
