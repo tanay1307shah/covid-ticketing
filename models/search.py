11daf2b2ac5e3adb06d63be1e7e4706e82ac98b2
tanshah <tanshah@redhat.com>
2020-06-22 10:37:46 -0700

    removed reduandant model

--- models/search.py
+++
@@ -1,6 +0,0 @@
-from flask_restplus import fields
-def searchModel(api):
-    searchModel = api.model("search",{
-        "searchTerm" : fields.String(required=True,description="search term to be searched for.")
-    })
-    return searchModel
