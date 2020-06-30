e0e0f2673ed583a25b9666959ea3c84bbe85f5ed
Binoy Subhakumar <subhakum@usc.edu>
2020-06-29 23:10:07 -0500

    added sending twilio message on delete reservation

--- helpers/sendMessage.py
+++ helpers/sendMessage.py
@@ -3,17 +3,18 @@ from twilio.rest import Client
 # Your Account SID from twilio.com/console
 account_sid = "ACcae58d44621ec3dde4529f074ae428be"
 # Your Auth Token from twilio.com/console
-auth_token  = "458c28a9f97e2197bc5354c04443bc52"
+auth_token = "458c28a9f97e2197bc5354c04443bc52"
 
 
-def sendMessage(personName,number,storeName,date, startTime, endTime):
-
-    client = Client(account_sid, auth_token)
-
-    message = client.messages.create(
-        to= "+1"+number, 
-        from_="+12565007953",
-        body="Hello," + personName + " your reservation for " + storeName + " from " + startTime + " till " + endTime + " on " + date + " is confirmed"
-    )
-
-    print(message.sid)
+def sendMessage(personName, number, storeName, date, startTime, endTime, message):
+    try:
+        client = Client(account_sid, auth_token)
+        message = client.messages.create(
+            to="+1"+number,
+            from_="+12565007953",
+            body="Hello," + personName + " your reservation for " + storeName +
+            " from " + startTime + " till " + endTime + " on " + date + " " + message
+        )
+        print(message + " is sent")
+    except Exception as e:
+        print("Error occurred:", str(e.args))
